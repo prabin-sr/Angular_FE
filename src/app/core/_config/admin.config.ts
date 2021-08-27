97b1a0b45deb97d8b2e8cb074901f541de54279b
Prabin <prabin.s106@gmail.com>
2021-08-27 22:18:05 +0530

    Initial commit

---
+++ src/app/core/_config/admin.config.ts
@@ -0,0 +1,11 @@
+export class admin_config {
+  public static model_quality = ['1', '2', '3']
+
+  public static plan = ['BASIC', 'STANDARD', 'PREMIUM' , 'CUSTOM']
+
+  public static reasons: string[] = ["This is temporary. I'll be back.", "I have a privacy concern.", 
+  "I don't find APP useful.", "My account was hacked.", "I get too many emails from APP.",
+  "I don't understand how to use APP.","I don't feel safe on APP.","I have another APP account.",
+  "Other, please explain further:"
+]
+}
